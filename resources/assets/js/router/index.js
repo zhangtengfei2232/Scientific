7528544596d5e09b12d4b84082a0afc107f23790
gongyijie <2590911884@qq.com>
2018-10-18 17:40:06 +0800

    index.js

--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -64,6 +64,70 @@ export default new VueRouter({
                 path: '/selfBook',
                 component: resolve => void(require(['../components/book/selfBook.vue'], resolve))
             },
+             /*获奖信息*/
+             {
+                name: 'award',
+                path: '/award',
+                component: resolve => void(require(['../components/award/book.vue'], resolve))
+            },
+            {
+                name: 'addAward',
+                path: '/addAward',
+                component: resolve => void(require(['../components/award/addAward.vue'], resolve))
+            },
+            {
+                name: 'selfAward',
+                path: '/selfAward',
+                component: resolve => void(require(['../components/award/selfAward.vue'], resolve))
+            },
+            /*专利信息*/
+            {
+                name: 'patent',
+                path: '/patent',
+                component: resolve => void(require(['../components/patent/patent.vue'], resolve))
+            },
+            {
+                name: 'addPatent',
+                path: '/addPatent',
+                component: resolve => void(require(['../components/patent/addPatent.vue'], resolve))
+            },
+            {
+                name: 'selfPatent',
+                path: '/selfPatent',
+                component: resolve => void(require(['../components/patent/selfPatent.vue'], resolve))
+            },
+            /*成果鉴定信息*/
+            {
+                name: 'appraisal',
+                path: '/appraisal',
+                component: resolve => void(require(['../components/appraisal/appraisal.vue'], resolve))
+            },
+            {
+                name: 'addAppraisal',
+                path: '/addAppraisal',
+                component: resolve => void(require(['../components/appraisal/addAppraisal.vue'], resolve))
+            },
+            {
+                name: 'selfAppraisal',
+                path: '/selfAppraisal',
+                component: resolve => void(require(['../components/appraisal/selfAppraisal.vue'], resolve))
+            },
+            /*举办会议信息*/
+            {
+                name: 'holdmeet',
+                path: '/holdmeet',
+                component: resolve => void(require(['../components/holdmeet/holdmeet.vue'], resolve))
+            },
+            {
+                name: 'addHoldmeet',
+                path: '/addHoldmeet',
+                component: resolve => void(require(['../components/holdmeet/addHoldmeet.vue'], resolve))
+            },
+            {
+                name: 'selfHoldmeet',
+                path: '/selfHoldmeet',
+                component: resolve => void(require(['../components/holdmeet/selfHoldmeet.vue'], resolve))
+            },
       ]
 
 })
