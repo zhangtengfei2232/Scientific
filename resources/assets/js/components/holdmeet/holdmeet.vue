ccbe3f14f60ec94e7e2d60c98c1323bfb755239e
gongyijie <2590911884@qq.com>
2018-11-17 20:27:41 +0800

    mmm

--- resources/assets/js/components/holdmeet/holdmeet.vue
+++ resources/assets/js/components/holdmeet/holdmeet.vue
@@ -282,7 +282,7 @@
                 });
             },
             deleteHoldmeetData(ho_id) {
-                let id = ho_id;
+                this.id.push(ho_id);
                 this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
@@ -291,7 +291,7 @@
                     let self = this;
                     axios.get("deleteholdmeet",{
                         params:{
-                            ho_id_datas:id
+                            ho_id_datas:self.id
                         }
                     }).then(function (response) {
                     var data = response.data;
