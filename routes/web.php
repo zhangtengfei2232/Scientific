8a69640351dba0319360e2a21a79c81551fabc5e
zhangtengfei2232 <2232050718@qq.com>
2018-11-26 21:29:03 +0800

    2018-11-26-21修改项目级别路由

--- routes/web.php
+++ routes/web.php
@@ -269,6 +269,7 @@ Route::group(['namespace' => 'ScientificController','middleware'=>['validate']],
      */
     Route::get('groupbyprojectcertificatelevel','RetrievalController@groupByProjectCertificateLevel');//按照项目学科门类分组查询
     Route::get('groupbyprojetcateresearch','RetrievalController@groupByProjetCateResearch');          //按照项目研究类别分组查询
+    Route::get('groupbyprojectlevel','RetrievalController@groupByProjectLevel');                      //项目级别查询
     /**
      * 著作数据路由
      */
